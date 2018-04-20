e880ec940dd279a9823f7257b26711b92dc31640
Bob Pascazio <bob.pascazio@agoda.com>
2018-04-20 08:56:26 +0700

    initial files

---
+++ install.sh
@@ -0,0 +1,8 @@
+#~/bin/sh
+cp ___bash_profile ~/.bash_profile
+cp ___bashrc ~/.bashrc
+cp ___emacs ~/.emacs
+cp __git-completion.bash ~/.git-completion.bash
+cp __git-prompt.sh ~/.git-prompt.sh
+cp __gitignore_global ~/.gitignore_global
+cp _emacs.d/lisp/* ~/.emacs.d/lisp
